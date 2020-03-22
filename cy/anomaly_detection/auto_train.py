498bc50483c2f94492edccf5d8f8d8a927d7b1c9
KingShineK <672843916@qq.com>
2020-03-22 09:48:58 +0800

    修改-传入参数增加topic,target,scene,out,start_time,end_time

--- cy/anomaly_detection/auto_train.py
+++ cy/anomaly_detection/auto_train.py
@@ -1,7 +1,13 @@
 # -*- coding:utf-8 -*-
 """
-本程序基于train.sh
-只需传入一个参数：interval，执行训练的时间间隔，单位为“日”
+本程序基于train.sh,需传入7个参数：
+    interval，执行训练的时间间隔，单位为“日”
+    topic，对应 kafka 的 topic
+    target，指标
+    scene，场景
+    out，训练模型输出路径
+    start_time，模型数据开始时间
+    end_time，模型数据截止时间
 """
 import os
 import sys
@@ -23,9 +29,16 @@ def run(command,interval):
 if __name__ == "__main__":
     # 间隔时间为日
     interval = sys.argv[1]
+    topic = sys.argv[2]
+    target = sys.argv[3]
+    scene = sys.argv[4]
+    out = sys.argv[5]
+    start_time = sys.argv[6]
+    end_time = sys.argv[7]
 
-    command = 'sh train.sh'
+    command = 'sh train.sh' + ' ' + topic + ' ' + target + ' ' + scene + ' ' + \
+              out + ' ' + start_time + ' ' + end_time
     interval = int(interval) * 24 * 3600
     # 刚开始的时候执行一次
     os.system(command)
-    run(command,interval)
+    run(command, interval)
