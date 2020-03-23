dd577a59193891cf4ff07e9ab6f42ae1a8355b5d
KingShineK <672843916@qq.com>
2020-03-23 16:58:49 +0800

    修改-将待训练的key放入单独的文件中，每次训练文件中全部。脚本参数改为四个：interval、out、start_time、end_time

--- cy/anomaly_detection/auto_train.py
+++ cy/anomaly_detection/auto_train.py
@@ -1,44 +1,49 @@
 # -*- coding:utf-8 -*-
 """
-本程序基于train.sh,需传入7个参数：
+本程序基于train.sh,需传入4个参数：
     interval，执行训练的时间间隔，单位为“日”
-    topic，对应 kafka 的 topic
-    target，指标
-    scene，场景
     out，训练模型输出路径
     start_time，模型数据开始时间
     end_time，模型数据截止时间
+topic、target、scene，从文件获取
 """
 import os
 import sys
 import time
 
-def run(command,interval):
+def run(train_file, interval, out, start_time, end_time):
 
-    before_time = time.time()
+    before_time = 0
     while True:
         try:
             local_time = time.time()
             # print(local_time)
             if local_time - before_time >= interval:
-                os.system(command)
+                train(train_file, out, start_time, end_time)
                 before_time = local_time
         except Exception as e:
             print(e)
+def train(train_file, out, start_time, end_time):
+    with open(train_file, encoding='utf-8') as f:
+        content = f.readlines()
+        train_list = [c.replace('\n', '') for c in content]
+
+    for row in train_list:
+        [topic, target, scene] = row.split('|')
+        command = 'sh train.sh' + ' ' + topic + ' ' + target + ' ' + scene + ' ' + \
+                  out + ' ' + start_time + ' ' + end_time
+        # 执行训练程序
+        print('开始训练主题为%s，指标为%s，场景为%s的模型。。。'%(topic,target,scene))
+        os.system(command)
 
 if __name__ == "__main__":
     # 间隔时间为日
     interval = sys.argv[1]
-    topic = sys.argv[2]
-    target = sys.argv[3]
-    scene = sys.argv[4]
-    out = sys.argv[5]
-    start_time = sys.argv[6]
-    end_time = sys.argv[7]
+    out = sys.argv[2]
+    start_time = sys.argv[3]
+    end_time = sys.argv[4]
 
-    command = 'sh train.sh' + ' ' + topic + ' ' + target + ' ' + scene + ' ' + \
-              out + ' ' + start_time + ' ' + end_time
-    interval = int(interval) * 24 * 3600
-    # 刚开始的时候执行一次
-    os.system(command)
-    run(command, interval)
+    train_file = 'auto_train_list'
+    interval = float(interval) * 24 * 3600
+
+    run(train_file, interval, out, start_time, end_time)
